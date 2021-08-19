46e7b76017c7b4d42ec92ef09769b2244bd695c7
Fatima biteye <zahrabiteye9@gmail.com>
2021-08-20 01:11:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+////require_once('connect_db.php');
+////addStudents();
+////?>
+<!--<!DOCTYPE html>-->
+<!--<html lang="en">-->
+<!--<head>-->
+<!--	<meta charset="UTF-8">-->
+<!--	<title>Ecole</title>-->
+
+<!--	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">-->
+<!--</head>-->
+<!--<body>-->
+<!--<div class="container border col-sm-4 mt-5">-->
+<!--	<h3 class="text-center text-info mt-5">Remplissage du formulaire </h3>-->
+<!--	<form class="form-inline" action="#" method="POST" onsubmit="return validate()">-->
+<!--		<div class="mb-3">-->
+<!--			<label for="matricule" class="form-label">Matricule</label>-->
+<!--			<input type="text" name="matricule" class="form-control" id="matricule" placeholder="Etu12365">-->
+<!--			<span id="matriculeloc" class="text-danger"></span>-->
+<!--		</div>-->
+<!--		<div class="mb-3">-->
+<!--			<label for="firstname" class="form-label">Prénom</label>-->
+<!--			<input type="text" name="firstname" class="form-control" id="firstname" placeholder="Fatima">-->
+<!---->
+<!--		</div>-->
+<!--		<div class="mb-3">-->
+<!--			<label for="lastname" class="form-label">Nom</label>-->
+<!--			<input type="text" name="lastname" class="form-control" id="lastname" placeholder="Diallo">-->
+<!---->
+<!--		</div>-->
+<!--		<div class="mb-3">-->
+<!--			<label for="birthday" class="form-label">Date de naissance</label>-->
+<!--			<input type="date" name="birthday" class="form-control" id="birthday">-->
+<!---->
+<!--		</div>-->
+<!--		<div class="mb-3 text-center">-->
+<!--			<button type="button" class="btn btn-lg btn-info " id="save">Enregistrer</button>-->
+<!--		</div>-->
+<!---->
+<!--	</form>-->
+<!--	<button class="btn btn-default btn-lg"><a href="subject.php">Matière</a></button>-->
+<!--</div>-->
+<!--<script src="script.js"></script>-->
+<!--</body>-->
+<!--</html>-->
+<!---->
+<!---->
+<!---->
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+<div>fatou</div>
+<p class="header">demba</p>
+
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,53 @@
+
+// function validate() {
+//     let matricule  = document.getElementById('matricule').value;
+//     let firstname  = document.getElementById('firstname').value;
+//     let lastname  = document.getElementById('lastname').value;
+//     let birthday  = document.getElementById('birthday').value;
+//     var status = false;
+//
+//   // test matricule
+//     if (matricule.length < 1){
+//         document.getElementById('matriculeloc').innerHTML = "Veuillez saisir un matricule svp!"
+//         status = false;
+//     }else{
+//         document.getElementById('matriculeloc').innerHTML =""
+//         status = true;
+//     }
+//
+//     // test firstname
+//     if (matricule.length < 1){
+//         document.getElementById('firstnameloc').innerHTML = "Veuillez saisir un nom svp!"
+//         status = false;
+//     }else{
+//         document.getElementById('firstnameloc').innerHTML =""
+//         status = true;
+//     }
+//
+//     // test lastname
+//     if (matricule.length < 1){
+//         document.getElementById('lastname').innerHTML = "Veuillez saisir un prénom svp!"
+//         status = false;
+//     }else{
+//         document.getElementById('lastname').innerHTML =""
+//         status = true;
+//     }
+//
+//     // test date naissance
+//     if (matricule.length < 1){
+//         document.getElementById('birthdayeloc').innerHTML = "Veuillez saisir une date de naissance svp!"
+//         status = false;
+//     }else{
+//         document.getElementById('birthdayeloc').innerHTML =""
+//         status = true;
+//     }
+//
+//
+//     return status
+// }
+
+$(document).ready(function () {
+    //$('div').show();
+    $(".header", $("p")).hide();
+
+});
